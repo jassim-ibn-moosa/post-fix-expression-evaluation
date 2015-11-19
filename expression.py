f508f7b39e128a89212fb9a2e599a23009772fd0
Mohamed Jassim <jassim.al.azmi@gmail.com>
2015-11-19 20:45:41 +0300

    Adding scan expression function to check whether the scannedelement is a operand or operator

--- expression.py
+++ expression.py
@@ -2,3 +2,11 @@ def get_post_expr():
     print "Enter the post fix expression"
     post_expr = raw_input()
     return post_expr
+
+def scan_expression(post_expr,operator,operand,i):
+    if post_expr[i] in operator:
+        operator = post_expr[i]
+        return operator
+    else:
+        operand = post_expr[i]
+        return operand
